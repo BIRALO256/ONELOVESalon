e131d5fef4b20464bf1e8961e8fc5f80dc26a683
MUWANGUZI JOVIC BIRALO <95884686+BIRALO256@users.noreply.github.com>
2024-02-21 22:36:31 +0300

    modified some stuff

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,18 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"jovic-white": "#ffffff",
+				"xephas-navy": "#000525",
+				"xephas-yellow": "#fbaa00",
+				"jovic": "#050505",
+				"xephas-hover":"#1126bf"
+			  },
+		},
+		fontFamily: {
+			mont: ["Montserrat", "sans-serif"],
+		  },
 	},
 	plugins: [],
 }
